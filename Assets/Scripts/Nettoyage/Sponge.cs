a692a0c017d90e1eb7cd8820234333f9746e0b22
TOUL Xochil <nix.axius@gmail.com>
2023-02-27 16:20:57 +0100

    shader images for nettoyage + eponge script

--- Assets/Scripts/Nettoyage/Sponge.cs
+++ Assets/Scripts/Nettoyage/Sponge.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Sponge : MonoBehaviour
 {
-
+    public GameObject particles;
     // Update is called once per frame
     void Update()
     {
         Vector3 mousePosition = Input.mousePosition;
         mousePosition.z = 0.1f;
         transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+
+    }
+
+    private void OnMouseDown()
+    {
+        particles.SetActive(true);
+    }
+
+    private void OnMouseUp()
+    {
+        particles.SetActive(false);
     }
 }
