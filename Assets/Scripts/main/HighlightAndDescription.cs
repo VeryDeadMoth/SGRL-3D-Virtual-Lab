75d5250b3c5de6d5905538f01b01f06d25214500
TOUL Xochil <nix.axius@gmail.com>
2023-03-02 18:03:41 +0100

    debut popup UI + outline pour objets

---
+++ Assets/Scripts/main/HighlightAndDescription.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HighlightAndDescription : MonoBehaviour
+{
+
+    private void OnMouseEnter()
+    {
+        
+    }
+
+    private void OnMouseExit()
+    {
+        
+    }
+}
