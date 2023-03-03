3da189e8f5e75c909d2cf5f059be689cb3a4be43
TOUL Xochil <nix.axius@gmail.com>
2023-03-03 15:54:13 +0100

    OUTLINE (super trop la classe)

--- Assets/Scripts/ToolTip.cs
+++ Assets/Scripts/ToolTip.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class ToolTip : MonoBehaviour
 {
     public string message;
+
+    private void Start()
+    {
+        transform.GetComponent<Outline>().enabled = false;
+    }
     private void OnMouseEnter()
     {
         ToolTipManager._instance.SetAndShowToolTip(message);
+
+        transform.GetComponent<Outline>().enabled = true;
     }
 
     private void OnMouseExit()
     {
         ToolTipManager._instance.HideToolTip();
+
+        transform.GetComponent<Outline>().enabled = false;
     }
 }
