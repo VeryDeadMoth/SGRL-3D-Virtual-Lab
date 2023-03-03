bbcc3a5412a446d8346eabe1713259c138600f21
pp <a.armar2003@fmail.com>
2023-03-03 12:04:38 +0100

    Debut Tooltip

---
+++ Assets/Scripts/ToolTipManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ToolTipManager : MonoBehaviour
+{
+    public static ToolTipManager _instance;
+    public TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        if (_instance!= null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+    public void SetAndShowToolTip(string message)
+    {
+        gameObject.SetActive(true);
+        text.text = message;
+    }
+
+    public void HideToolTip()
+    {
+        gameObject.SetActive(false);
+        text.text = "";
+    }
+}
